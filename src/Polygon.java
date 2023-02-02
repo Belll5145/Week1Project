f66f566b6a7d2d34259fb955ef2a5809b52a1c43
Belll5145 <Belll@wlhs.wlwv.k12.or.us>
2023-02-02 09:53:08 -0800

    Created default constructor

---
+++ src/Polygon.java
@@ -0,0 +1,17 @@
+/**
+ * @file Polygon.java
+ * @author Landon Bell
+ * @date 2/2/23
+ */
+public class Polygon {
+    private int numSides;
+    private double sideLength;
+    private String type;
+
+
+    public Polygon(){
+       numSides = 4;
+       sideLength = 1.0;
+       type = "square";
+    }
+}
---
+++ src/PolygonTester.java
@@ -0,0 +1,8 @@
+/**
+ * @file PolygonTester.java
+ * @author Landon Bell
+ * @date 2/2/23
+ */
+public class PolygonTester {
+
+}
