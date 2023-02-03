0e30ae2cd0afba311a4d49a38af77680653a90c7
Belll5145 <113708748+Belll5145@users.noreply.github.com>
2023-02-03 19:49:21 +0000

    Finished week 1

--- src/Polygon.java
+++ src/Polygon.java
@@ -8,10 +8,51 @@ public class Polygon {
     private double sideLength;
     private String type;
 
-
+    // default constructor
     public Polygon(){
        numSides = 4;
        sideLength = 1.0;
        type = "square";
     }
+  /**
+  * Creates a Polygon object with specified sides, side lengths,
+  * and shape type. If the number of sides is 0 or less, it is
+  * is set to 4. If the side lengths are 0 or less, it is set to
+  * 1.0.
+  */
+    public Polygon(int sides, double length, String shape){
+      numSides = 4;
+      sideLength = 1.0;
+      type = shape;
+      if(length > 0.0){
+        sideLength = length;
+      }
+      if(sides > 0){
+        numSides = sides;
+      }
+    }
+    /**
+    * 
+`   *@return the number of sides of the polygon
+    */
+    public int getNumSides(){
+      return numSides;
+    }
+    /**
+    *
+    *@return the length of the sides of the polygon
+    */
+    public double getSideLength(){
+      return sideLength;
+    }
+    /**
+    *
+    *@return the type of polygon
+    */
+  public String getShapeType(){
+    return type;
+  }
+  public String toString(){
+    return "Your shape is a "+ type + " and it has "+ numSides +" sides.\n It has a side length of "+ sideLength +" units.";
+  }
 }
--- src/PolygonTester.java
+++ src/PolygonTester.java
@@ -4,5 +4,17 @@
  * @date 2/2/23
  */
 public class PolygonTester {
+  public static void main(String[] args) {
+        
+        Polygon poly1 = new Polygon(5,6.865,"pentagon");
+        System.out.println(poly1);
 
+        
+        Polygon poly2 = new Polygon();
+        System.out.println(poly2);
+
+        
+        Polygon poly3 = new Polygon(1,785,"line");
+        System.out.println(poly3);
+    }
 }
